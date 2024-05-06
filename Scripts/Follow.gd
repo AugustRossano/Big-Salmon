af75cc3ef58cc17c7a2adda1ba2fbf6c0bfa6599
Vincent Hayes <Vincent.Hayes@Digipen.edu>
2024-05-06 14:21:46 -0700

    Follow script wip1

---
+++ Scripts/Follow.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+# Variables to store the target object and speed
+var target : Node3D
+var speed : float = 100
+
+func _process(delta):
+	# Check if the target is valid
+	if target:
+		# Calculate the direction towards the target
+		var direction = (target.global_position - global_position).normalized()
+	   
+		# Move towards the target
+		global_position += direction * speed * delta
+	else:
+		print("No target set!")
+
+# Function to set the target object
+func set_target(new_target : Node3D):
+	target = new_target
